923066808fe6b2f900e1222b966aead134eb8c2b
g1f4n <muhammadhusain1996@gmail.com>
2020-04-02 18:01:32 +0700

    initial commit

---
+++ pi_face_recognition.py
@@ -0,0 +1,159 @@
+# USAGE
+# python pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+import os
+from time import strftime
+import requests
+from PIL import Image
+from six import StringIO
+import json
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--cascade", required=True,
+	help = "path to where the face cascade resides")
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+args = vars(ap.parse_args())
+
+# load the known faces and embeddings along with OpenCV's Haar
+# cascade for face detection
+print("[INFO] loading encodings + face detector...")
+data = pickle.loads(open(args["encodings"], "rb").read())
+detector = cv2.CascadeClassifier(args["cascade"])
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)
+total = 0
+
+# start the FPS counter
+fps = FPS().start()
+
+# API ENDPOINT
+# API_ENDPOINT = "http://192.168.43.95:5000/"
+content_type = 'image/jpeg'
+
+# loop over frames from the video file stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to 500px (to speedup processing)
+	frame = vs.read()
+	orig = frame.copy()
+	frame = imutils.resize(frame, width=500)
+	
+	# convert the input frame from (1) BGR to grayscale (for face
+	# detection) and (2) from BGR to RGB (for face recognition)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	# detect faces in the grayscale frame
+	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+		minNeighbors=5, minSize=(30, 30),
+		flags=cv2.CASCADE_SCALE_IMAGE)
+
+	# OpenCV returns bounding box coordinates in (x, y, w, h) order
+	# but we need them in (top, right, bottom, left) order, so we
+	# need to do a bit of reordering
+	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
+	# compute the facial embeddings for each face bounding box
+	encodings = face_recognition.face_encodings(rgb)
+	names = []
+
+	# loop over the facial embeddings
+	for encoding in encodings:
+		# attempt to match each face in the input image to our known
+		# encodings
+		matches = face_recognition.compare_faces(data["encodings"],
+			encoding)
+		name = "Unknown"
+
+		# check to see if we have found a match
+		if True in matches:
+			# find the indexes of all matched faces then initialize a
+			# dictionary to count the total number of times each face
+			# was matched
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			counts = {}
+
+			# loop over the matched indexes and maintain a count for
+			# each recognized face face
+			for i in matchedIdxs:
+				name = data["names"][i]
+				counts[name] = counts.get(name, 0) + 1
+
+			# determine the recognized face with the largest number
+			# of votes (note: in the event of an unlikely tie Python
+			# will select first entry in the dictionary)
+			name = max(counts, key=counts.get)
+		
+		# update the list of names
+		names.append(name)
+
+	# loop over the recognized faces
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+		# draw the predicted face name on the image
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 0), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			0.75, (0, 255, 0), 2)
+
+	cv2.putText(frame, strftime("%H:%M:%S"), (10,70), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,255,0),2,cv2.LINE_AA)
+	# display the image to our screen
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	if key == ord("k"):
+	# if strftime("%H:%M:%S") < "10:00:00":
+		# p = os.path.sep.join(['http://192.168.43.95:5000/absen', name+"{}.jpg".format(str(total).zfill(5))])
+		# cv2.imwrite(p, orig)
+		# save_img = cv2.imread('D:/Project Fix/Python Project/pi-face-recognition2/images/gifan00000.jpg')
+		# save_img = cv2.imwrite(p, frame)
+		# data2 = {
+		# 	'api_dev_key': '',
+		# 	'api_option': 'paste',
+		# 	'api_paste_code': save_img,
+		# 	'api_paste_format': 'python'
+		# }
+		
+		_, data_wajah = cv2.imencode('.jpg', orig)
+
+		username = name+"{}.jpg".format(str(total).zfill(5))
+		# send http request with image and receive response
+		response = requests.post('http://192.168.43.95:5000/absen', data=data_wajah.tostring())
+		# response = requests.post('http://192.168.43.95:5000/absen', data=username)
+		
+		# r = requests.post(url = 'http://192.168.43.95:5000/absen', data = data2)
+		print(response.text)
+		total += 1
+		
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
